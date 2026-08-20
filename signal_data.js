3bef8c7244d965181e4b85fed88fed6c18c7a852
lowbuy-publisher <lowbuy@localhost>
2026-08-20 10:08:17 +0800

    auto update

---
+++ signal_data.js
@@ -0,0 +1,18 @@
+window.SIGNAL_DATA = {
+ "generated": "2026-08-20 09:13",
+ "today": "20260820",
+ "days": [
+  {
+   "date": "20260820",
+   "candidates": [
+    {
+     "code": "002678",
+     "date": "2026-08-19",
+     "close": 4.71,
+     "emotion": "退潮期"
+    }
+   ],
+   "sells": []
+  }
+ ]
+};
