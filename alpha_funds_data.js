e7bbdf2da918ab7cecc5518ad9e7c2ea4dfee57e
lowbuy-publisher <lowbuy@localhost>
2026-09-03 09:43:22 +0800

    auto update

---
+++ alpha_funds_data.js
@@ -0,0 +1,374 @@
+window.ALPHA_FUNDS_DATA = {
+  "generated": "2026-09-03 09:36:51",
+  "today": "20260903",
+  "days": [
+    {
+      "date": "20260903",
+      "candidates": [
+        {
+          "code": "601609",
+          "name": "金田股份",
+          "date": "20260903",
+          "buyDate": "20260904",
+          "buyPrice": "",
+          "close": 13.54,
+          "emotion": "",
+          "state": "待次日开盘",
+          "sellDate": "",
+          "sellPrice": "",
+          "reason": "",
+          "pnl": "",
+          "period": ""
+        },
+        {
+          "code": "002437",
+          "name": "誉衡药业",
+          "date": "20260903",
+          "buyDate": "20260904",
+          "buyPrice": "",
+          "close": 4.29,
+          "emotion": "",
+          "state": "待次日开盘",
+          "sellDate": "",
+          "sellPrice": "",
+          "reason": "",
+          "pnl": "",
+          "period": ""
+        }
+      ],
+      "sells": []
+    },
+    {
+      "date": "20260902",
+      "candidates": [
+        {
+          "code": "601609",
+          "name": "金田股份",
+          "date": "20260902",
+          "buyDate": "20260903",
+          "buyPrice": "",
+          "close": 13.46,
+          "emotion": "",
+          "state": "待次日开盘",
+          "sellDate": "",
+          "sellPrice": "",
+          "reason": "",
+          "pnl": "",
+          "period": ""
+        }
+      ],
+      "sells": []
+    },
+    {
+      "date": "20260901",
+      "candidates": [
+        {
+          "code": "601609",
+          "name": "金田股份",
+          "date": "20260901",
+          "buyDate": "20260902",
+          "buyPrice": "",
+          "close": 13.46,
+          "emotion": "退潮期",
+          "state": "退潮期不买",
+          "sellDate": "",
+          "sellPrice": "",
+          "reason": "次日退潮不买",
+          "pnl": "",
+          "period": ""
+        }
+      ],
+      "sells": []
+    },
+    {
+      "date": "20260831",
+      "candidates": [
+        {
+          "code": "601609",
+          "name": "金田股份",
+          "date": "20260831",
+          "buyDate": "20260901",
+          "buyPrice": "",
+          "close": 13.49,
+          "emotion": "混沌期",
+          "state": "待次日开盘",
+          "sellDate": "",
+          "sellPrice": "",
+          "reason": "",
+          "pnl": "",
+          "period": ""
+        }
+      ],
+      "sells": []
+    },
+    {
+      "date": "20260828",
+      "candidates": [
+        {
+          "code": "601609",
+          "name": "金田股份",
+          "date": "20260828",
+          "buyDate": "20260831",
+          "buyPrice": "",
+          "close": 12.98,
+          "emotion": "混沌期",
+          "state": "待次日开盘",
+          "sellDate": "",
+          "sellPrice": "",
+          "reason": "",
+          "pnl": "",
+          "period": ""
+        },
+        {
+          "code": "000620",
+          "name": "盈新发展",
+          "date": "20260828",
+          "buyDate": "20260831",
+          "buyPrice": "",
+          "close": 3.38,
+          "emotion": "混沌期",
+          "state": "待次日开盘",
+          "sellDate": "",
+          "sellPrice": "",
+          "reason": "",
+          "pnl": "",
+          "period": ""
+        }
+      ],
+      "sells": []
+    },
+    {
+      "date": "20260827",
+      "candidates": [
+        {
+          "code": "600611",
+          "name": "大众交通",
+          "date": "20260827",
+          "buyDate": "20260828",
+          "buyPrice": "",
+          "close": 4.61,
+          "emotion": "混沌期",
+          "state": "待次日开盘",
+          "sellDate": "",
+          "sellPrice": "",
+          "reason": "",
+          "pnl": "",
+          "period": ""
+        },
+        {
+          "code": "601609",
+          "name": "金田股份",
+          "date": "20260827",
+          "buyDate": "20260828",
+          "buyPrice": "",
+          "close": 13.35,
+          "emotion": "混沌期",
+          "state": "待次日开盘",
+          "sellDate": "",
+          "sellPrice": "",
+          "reason": "",
+          "pnl": "",
+          "period": ""
+        },
+        {
+          "code": "000620",
+          "name": "盈新发展",
+          "date": "20260827",
+          "buyDate": "20260828",
+          "buyPrice": "",
+          "close": 3.45,
+          "emotion": "混沌期",
+          "state": "待次日开盘",
+          "sellDate": "",
+          "sellPrice": "",
+          "reason": "",
+          "pnl": "",
+          "period": ""
+        }
+      ],
+      "sells": []
+    },
+    {
+      "date": "20260826",
+      "candidates": [
+        {
+          "code": "600611",
+          "name": "大众交通",
+          "date": "20260826",
+          "buyDate": "20260827",
+          "buyPrice": "",
+          "close": 4.67,
+          "emotion": "混沌期",
+          "state": "待次日开盘",
+          "sellDate": "",
+          "sellPrice": "",
+          "reason": "",
+          "pnl": "",
+          "period": ""
+        },
+        {
+          "code": "601609",
+          "name": "金田股份",
+          "date": "20260826",
+          "buyDate": "20260827",
+          "buyPrice": "",
+          "close": 13.17,
+          "emotion": "混沌期",
+          "state": "待次日开盘",
+          "sellDate": "",
+          "sellPrice": "",
+          "reason": "",
+          "pnl": "",
+          "period": ""
+        }
+      ],
+      "sells": []
+    },
+    {
+      "date": "20260825",
+      "candidates": [],
+      "sells": []
+    },
+    {
+      "date": "20260824",
+      "candidates": [],
+      "sells": []
+    },
+    {
+      "date": "20260821",
+      "candidates": [],
+      "sells": []
+    },
+    {
+      "date": "20260820",
+      "candidates": [],
+      "sells": []
+    },
+    {
+      "date": "20260819",
+      "candidates": [],
+      "sells": []
+    },
+    {
+      "date": "20260818",
+      "candidates": [
+        {
+          "code": "002900",
+          "name": "哈三联",
+          "date": "20260818",
+          "buyDate": "20260819",
+          "buyPrice": "",
+          "close": 14.21,
+          "emotion": "退潮期",
+          "state": "退潮期不买",
+          "sellDate": "",
+          "sellPrice": "",
+          "reason": "次日退潮不买",
+          "pnl": "",
+          "period": ""
+        },
+        {
+          "code": "600611",
+          "name": "大众交通",
+          "date": "20260818",
+          "buyDate": "20260819",
+          "buyPrice": "",
+          "close": 4.67,
+          "emotion": "退潮期",
+          "state": "退潮期不买",
+          "sellDate": "",
+          "sellPrice": "",
+          "reason": "次日退潮不买",
+          "pnl": "",
+          "period": ""
+        }
+      ],
+      "sells": []
+    },
+    {
+      "date": "20260817",
+      "candidates": [],
+      "sells": []
+    },
+    {
+      "date": "20260814",
+      "candidates": [],
+      "sells": []
+    },
+    {
+      "date": "20260813",
+      "candidates": [],
+      "sells": []
+    },
+    {
+      "date": "20260812",
+      "candidates": [],
+      "sells": []
+    },
+    {
+      "date": "20260811",
+      "candidates": [],
+      "sells": []
+    },
+    {
+      "date": "20260810",
+      "candidates": [],
+      "sells": []
+    },
+    {
+      "date": "20260807",
+      "candidates": [],
+      "sells": []
+    },
+    {
+      "date": "20260806",
+      "candidates": [],
+      "sells": []
+    },
+    {
+      "date": "20260805",
+      "candidates": [],
+      "sells": []
+    },
+    {
+      "date": "20260804",
+      "candidates": [],
+      "sells": []
+    },
+    {
+      "date": "20260803",
+      "candidates": [],
+      "sells": []
+    },
+    {
+      "date": "20260731",
+      "candidates": [],
+      "sells": []
+    },
+    {
+      "date": "20260730",
+      "candidates": [],
+      "sells": []
+    },
+    {
+      "date": "20260729",
+      "candidates": [],
+      "sells": []
+    },
+    {
+      "date": "20260728",
+      "candidates": [],
+      "sells": []
+    },
+    {
+      "date": "20260727",
+      "candidates": [],
+      "sells": []
+    },
+    {
+      "date": "20260724",
+      "candidates": [],
+      "sells": []
+    }
+  ]
+};
