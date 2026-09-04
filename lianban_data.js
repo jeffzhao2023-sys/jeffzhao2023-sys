b3abae74f1ddfceb7a1fd8d8ed3484acb0035a5e
lowbuy-publisher <lowbuy@localhost>
2026-09-04 14:11:50 +0800

    新增连板低吸看板栏

---
+++ lianban_data.js
@@ -0,0 +1,366 @@
+window.LIANBAN_DATA = {
+  "generated": "2026-09-04 14:08:49",
+  "today": "2026-09-04",
+  "pool": [
+    {
+      "code": "sh601086",
+      "name": "国芳集团",
+      "boards": 5,
+      "breakDate": "2026-09-04",
+      "lastClose": 12.87,
+      "ma5": 11.088,
+      "ma10": 9.39,
+      "status": "观察中"
+    },
+    {
+      "code": "sz002909",
+      "name": "集泰股份",
+      "boards": 4,
+      "breakDate": "2026-09-04",
+      "lastClose": 7.5,
+      "ma5": 7.086,
+      "ma10": 6.204,
+      "status": "观察中"
+    },
+    {
+      "code": "sz002297",
+      "name": "博云新材",
+      "boards": 2,
+      "breakDate": "2026-09-04",
+      "lastClose": 21.34,
+      "ma5": 20.009999999999998,
+      "ma10": 18.909,
+      "status": "观察中"
+    },
+    {
+      "code": "sz002708",
+      "name": "光洋股份",
+      "boards": 2,
+      "breakDate": "2026-09-04",
+      "lastClose": 15.41,
+      "ma5": 14.146,
+      "ma10": 13.044999999999998,
+      "status": "观察中"
+    },
+    {
+      "code": "sz002300",
+      "name": "太阳电缆",
+      "boards": 2,
+      "breakDate": "2026-09-04",
+      "lastClose": 8.0,
+      "ma5": 7.56,
+      "ma10": 7.165000000000001,
+      "status": "观察中"
+    },
+    {
+      "code": "sh603270",
+      "name": "金帝股份",
+      "boards": 2,
+      "breakDate": "2026-09-04",
+      "lastClose": 33.75,
+      "ma5": 31.919999999999998,
+      "ma10": 29.884000000000004,
+      "status": "观察中"
+    },
+    {
+      "code": "sz002059",
+      "name": "云南旅游",
+      "boards": 2,
+      "breakDate": "2026-09-04",
+      "lastClose": 5.94,
+      "ma5": 5.286,
+      "ma10": 4.929,
+      "status": "观察中"
+    },
+    {
+      "code": "sz003005",
+      "name": "竞业达",
+      "boards": 4,
+      "breakDate": "2026-09-03",
+      "lastClose": 18.7,
+      "ma5": 18.788,
+      "ma10": 16.330000000000002,
+      "status": "观察中"
+    },
+    {
+      "code": "sz000892",
+      "name": "欢瑞世纪",
+      "boards": 3,
+      "breakDate": "2026-09-03",
+      "lastClose": 5.59,
+      "ma5": 4.9799999999999995,
+      "ma10": 4.381,
+      "status": "观察中"
+    },
+    {
+      "code": "sh600892",
+      "name": "大晟文化",
+      "boards": 3,
+      "breakDate": "2026-09-03",
+      "lastClose": 4.91,
+      "ma5": 4.998,
+      "ma10": 4.473,
+      "status": "观察中"
+    },
+    {
+      "code": "sz002104",
+      "name": "恒宝股份",
+      "boards": 2,
+      "breakDate": "2026-09-03",
+      "lastClose": 14.12,
+      "ma5": 12.824000000000002,
+      "ma10": 12.186,
+      "status": "观察中"
+    },
+    {
+      "code": "sh600967",
+      "name": "内蒙一机",
+      "boards": 2,
+      "breakDate": "2026-09-03",
+      "lastClose": 13.68,
+      "ma5": 12.784,
+      "ma10": 11.925,
+      "status": "观察中"
+    },
+    {
+      "code": "sh601566",
+      "name": "九牧王",
+      "boards": 2,
+      "breakDate": "2026-09-03",
+      "lastClose": 10.82,
+      "ma5": 9.968,
+      "ma10": 9.17,
+      "status": "观察中"
+    },
+    {
+      "code": "sh600830",
+      "name": "香溢融通",
+      "boards": 2,
+      "breakDate": "2026-09-03",
+      "lastClose": 9.8,
+      "ma5": 9.546000000000001,
+      "ma10": 8.961,
+      "status": "观察中"
+    },
+    {
+      "code": "sh603207",
+      "name": "小方制药",
+      "boards": 2,
+      "breakDate": "2026-09-03",
+      "lastClose": 25.33,
+      "ma5": 25.541999999999998,
+      "ma10": 24.399,
+      "status": "观察中"
+    },
+    {
+      "code": "sh600828",
+      "name": "茂业商业",
+      "boards": 2,
+      "breakDate": "2026-09-03",
+      "lastClose": 5.2,
+      "ma5": 4.6899999999999995,
+      "ma10": 4.343,
+      "status": "观察中"
+    },
+    {
+      "code": "sz002514",
+      "name": "*ST宝馨",
+      "boards": 2,
+      "breakDate": "2026-09-03",
+      "lastClose": 2.49,
+      "ma5": 2.202,
+      "ma10": 1.983,
+      "status": "观察中"
+    },
+    {
+      "code": "sz000635",
+      "name": "英 力 特",
+      "boards": 2,
+      "breakDate": "2026-09-03",
+      "lastClose": 7.7,
+      "ma5": 7.37,
+      "ma10": 6.884,
+      "status": "观察中"
+    },
+    {
+      "code": "sz002084",
+      "name": "海鸥住工",
+      "boards": 7,
+      "breakDate": "2026-09-02",
+      "lastClose": 6.98,
+      "ma5": 6.882,
+      "ma10": 5.857,
+      "status": "观察中"
+    },
+    {
+      "code": "sh600371",
+      "name": "万向德农",
+      "boards": 6,
+      "breakDate": "2026-09-02",
+      "lastClose": 13.45,
+      "ma5": 13.52,
+      "ma10": 11.572,
+      "status": "观察中"
+    },
+    {
+      "code": "sz000560",
+      "name": "我爱我家",
+      "boards": 3,
+      "breakDate": "2026-09-02",
+      "lastClose": 3.15,
+      "ma5": 3.056,
+      "ma10": 2.728,
+      "status": "观察中"
+    },
+    {
+      "code": "sh600551",
+      "name": "时代出版",
+      "boards": 3,
+      "breakDate": "2026-09-02",
+      "lastClose": 8.09,
+      "ma5": 8.4,
+      "ma10": 7.698,
+      "status": "观察中"
+    },
+    {
+      "code": "sz002679",
+      "name": "福建金森",
+      "boards": 3,
+      "breakDate": "2026-09-02",
+      "lastClose": 10.81,
+      "ma5": 11.744,
+      "ma10": 10.756,
+      "status": "观察中"
+    },
+    {
+      "code": "sz300413",
+      "name": "芒果超媒",
+      "boards": 2,
+      "breakDate": "2026-09-02",
+      "lastClose": 21.2,
+      "ma5": 19.845999999999997,
+      "ma10": 16.972,
+      "status": "观察中"
+    },
+    {
+      "code": "sz001330",
+      "name": "博纳影业",
+      "boards": 2,
+      "breakDate": "2026-09-02",
+      "lastClose": 5.79,
+      "ma5": 5.832,
+      "ma10": 5.4079999999999995,
+      "status": "观察中"
+    },
+    {
+      "code": "sh600121",
+      "name": "郑州煤电",
+      "boards": 2,
+      "breakDate": "2026-09-02",
+      "lastClose": 5.12,
+      "ma5": 5.284000000000001,
+      "ma10": 4.756,
+      "status": "观察中"
+    },
+    {
+      "code": "sh603533",
+      "name": "掌阅科技",
+      "boards": 2,
+      "breakDate": "2026-09-02",
+      "lastClose": 24.08,
+      "ma5": 23.791999999999998,
+      "ma10": 22.21,
+      "status": "观察中"
+    },
+    {
+      "code": "sh603721",
+      "name": "中广天择",
+      "boards": 2,
+      "breakDate": "2026-09-02",
+      "lastClose": 21.27,
+      "ma5": 20.71,
+      "ma10": 19.16,
+      "status": "观察中"
+    },
+    {
+      "code": "sz000712",
+      "name": "锦龙股份",
+      "boards": 4,
+      "breakDate": "2026-09-01",
+      "lastClose": 12.58,
+      "ma5": 12.969999999999999,
+      "ma10": 11.504999999999999,
+      "status": "观察中"
+    },
+    {
+      "code": "sz002396",
+      "name": "星网锐捷",
+      "boards": 3,
+      "breakDate": "2026-09-01",
+      "lastClose": 37.16,
+      "ma5": 39.35,
+      "ma10": 36.003,
+      "status": "观察中"
+    },
+    {
+      "code": "sh603269",
+      "name": "海鸥股份",
+      "boards": 3,
+      "breakDate": "2026-09-01",
+      "lastClose": 27.51,
+      "ma5": 26.54,
+      "ma10": 23.003,
+      "status": "观察中"
+    },
+    {
+      "code": "sh600722",
+      "name": "金牛化工",
+      "boards": 2,
+      "breakDate": "2026-09-01",
+      "lastClose": 17.83,
+      "ma5": 17.856,
+      "ma10": 16.097,
+      "status": "观察中"
+    },
+    {
+      "code": "sh600691",
+      "name": "潞化科技",
+      "boards": 2,
+      "breakDate": "2026-09-01",
+      "lastClose": 3.03,
+      "ma5": 3.154,
+      "ma10": 2.92,
+      "status": "观察中"
+    },
+    {
+      "code": "sh603559",
+      "name": "中通国脉",
+      "boards": 2,
+      "breakDate": "2026-09-01",
+      "lastClose": 10.64,
+      "ma5": 10.714,
+      "ma10": 9.866,
+      "status": "观察中"
+    },
+    {
+      "code": "sh600479",
+      "name": "千金药业",
+      "boards": 3,
+      "breakDate": "2026-08-31",
+      "lastClose": 13.18,
+      "ma5": 13.184000000000001,
+      "ma10": 12.221,
+      "status": "观察中"
+    },
+    {
+      "code": "sh600654",
+      "name": "中安科",
+      "boards": 2,
+      "breakDate": "2026-08-31",
+      "lastClose": 3.25,
+      "ma5": 3.3299999999999996,
+      "ma10": 3.242,
+      "status": "观察中"
+    }
+  ]
+};
