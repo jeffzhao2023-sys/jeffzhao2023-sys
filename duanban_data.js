68d0b1bf65f55b67f8f0db860b7defa9791497cf
dashboard-publisher <dashboard@localhost>
2026-09-15 10:03:27 +0800

    auto update dashboard

--- duanban_data.js
+++ duanban_data.js
@@ -1,7 +1,27 @@
 window.DUANBAN_DATA = {
-  "generated": "2026-09-15 09:51:10",
+  "generated": "2026-09-15 10:03:23",
   "today": "20260915",
   "emotion": "混沌期",
   "alerts": [],
+  "candidates": [
+    {
+      "code": "000993",
+      "name": "闽东电力",
+      "boards": 4,
+      "prevClose": 15.27,
+      "runUp": 51.6,
+      "price": 16.18,
+      "pct": 5.96
+    },
+    {
+      "code": "000823",
+      "name": "超声电子",
+      "boards": 3,
+      "prevClose": 22.63,
+      "runUp": 56.2,
+      "price": 24.59,
+      "pct": 8.66
+    }
+  ],
   "pool": []
 };
