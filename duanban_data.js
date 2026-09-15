9f1e018e447d87fc78ec4815e03fb95240eea6ee
dashboard-publisher <dashboard@localhost>
2026-09-15 14:45:23 +0800

    auto update dashboard

--- duanban_data.js
+++ duanban_data.js
@@ -1,8 +1,19 @@
 window.DUANBAN_DATA = {
-  "generated": "2026-09-15 14:43:51",
+  "generated": "2026-09-15 14:44:52",
   "today": "20260915",
   "emotion": "混沌期",
-  "alerts": [],
+  "alerts": [
+    {
+      "time": "14:45:22",
+      "type": "断板观察",
+      "code": "000823",
+      "name": "超声电子",
+      "breakDate": "20260915",
+      "price": 23.93,
+      "note": "断板日涨幅5.74% 超出-5%~+5%，不买，等反包",
+      "emotion": "混沌期"
+    }
+  ],
   "candidates": [
     {
       "code": "000993",
