678b73072c1f5894b07c3fe2154ca8dfcfa09a3b
dashboard-publisher <dashboard@localhost>
2026-09-15 11:00:09 +0800

    auto update dashboard

--- lianban_data.js
+++ lianban_data.js
@@ -1,5 +1,5 @@
 window.LIANBAN_DATA = {
-  "generated": "2026-09-15 10:30:07",
+  "generated": "2026-09-15 11:00:08",
   "today": "2026-09-14",
   "pool": [
     {
@@ -7,7 +7,7 @@ window.LIANBAN_DATA = {
       "name": "鼎信通讯",
       "boards": 3,
       "breakDate": "2026-09-14",
-      "lastClose": 7.43,
+      "lastClose": 7.6,
       "buyPrice": 7.5,
       "ma5": null,
       "ma250": null,
@@ -49,7 +49,7 @@ window.LIANBAN_DATA = {
       "name": "九鼎新材",
       "boards": 2,
       "breakDate": "2026-09-14",
-      "lastClose": 11.02,
+      "lastClose": 10.86,
       "buyPrice": 11.22,
       "ma5": 10.17,
       "ma250": 10.310319999999999,
@@ -238,7 +238,7 @@ window.LIANBAN_DATA = {
       "name": "龙版传媒",
       "boards": 6,
       "breakDate": "2026-09-08",
-      "lastClose": 15.29,
+      "lastClose": 15.14,
       "buyPrice": 18.67,
       "ma5": 16.454,
       "ma250": 12.25884,
