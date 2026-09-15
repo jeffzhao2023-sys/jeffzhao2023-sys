8a2fa7503f1df72c4bff58cbf05c4cb1d5663abc
dashboard-publisher <dashboard@localhost>
2026-09-15 10:13:43 +0800

    auto update dashboard

--- duanban_data.js
+++ duanban_data.js
@@ -1,5 +1,5 @@
 window.DUANBAN_DATA = {
-  "generated": "2026-09-15 10:08:35",
+  "generated": "2026-09-15 10:13:38",
   "today": "20260915",
   "emotion": "混沌期",
   "alerts": [],
@@ -10,8 +10,8 @@ window.DUANBAN_DATA = {
       "boards": 4,
       "prevClose": 15.27,
       "runUp": 51.6,
-      "price": 16.13,
-      "pct": 5.63
+      "price": 16.35,
+      "pct": 7.07
     },
     {
       "code": "000823",
@@ -19,8 +19,8 @@ window.DUANBAN_DATA = {
       "boards": 3,
       "prevClose": 22.63,
       "runUp": 56.2,
-      "price": 24.52,
-      "pct": 8.35
+      "price": 24.19,
+      "pct": 6.89
     }
   ],
   "pool": []
