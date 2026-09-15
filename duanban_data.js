78af1f0930776faf5aa9501ef3dae9f3e04d1a51
dashboard-publisher <dashboard@localhost>
2026-09-15 13:30:08 +0800

    auto update dashboard

--- duanban_data.js
+++ duanban_data.js
@@ -1,5 +1,5 @@
 window.DUANBAN_DATA = {
-  "generated": "2026-09-15 13:26:20",
+  "generated": "2026-09-15 13:29:53",
   "today": "20260915",
   "emotion": "混沌期",
   "alerts": [],
@@ -19,8 +19,8 @@ window.DUANBAN_DATA = {
       "boards": 3,
       "prevClose": 22.63,
       "runUp": 56.2,
-      "price": 23.79,
-      "pct": 5.13
+      "price": 23.78,
+      "pct": 5.08
     }
   ],
   "records": []
--- lianban_data.js
+++ lianban_data.js
@@ -1,5 +1,5 @@
 window.LIANBAN_DATA = {
-  "generated": "2026-09-15 13:00:08",
+  "generated": "2026-09-15 13:30:07",
   "today": "2026-09-14",
   "pool": [
     {
@@ -7,7 +7,7 @@ window.LIANBAN_DATA = {
       "name": "鼎信通讯",
       "boards": 3,
       "breakDate": "2026-09-14",
-      "lastClose": 7.49,
+      "lastClose": 7.24,
       "buyPrice": 7.5,
       "ma5": null,
       "ma250": null,
@@ -49,7 +49,7 @@ window.LIANBAN_DATA = {
       "name": "九鼎新材",
       "boards": 2,
       "breakDate": "2026-09-14",
-      "lastClose": 10.86,
+      "lastClose": 10.82,
       "buyPrice": 11.22,
       "ma5": 10.17,
       "ma250": 10.310319999999999,
