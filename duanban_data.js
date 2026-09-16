7bef87faeaa1d2a5a820c33d4816e46c8fd7a57d
dashboard-publisher <dashboard@localhost>
2026-09-16 10:00:08 +0800

    auto update dashboard

--- duanban_data.js
+++ duanban_data.js
@@ -1,5 +1,5 @@
 window.DUANBAN_DATA = {
-  "generated": "2026-09-16 09:35:33",
+  "generated": "2026-09-16 09:37:05",
   "today": "20260916",
   "emotion": "混沌期",
   "alerts": [],
@@ -10,8 +10,8 @@ window.DUANBAN_DATA = {
       "boards": 5,
       "prevClose": 16.8,
       "runUp": 63.7,
-      "price": 18.1,
-      "pct": 7.74
+      "price": 18.48,
+      "pct": 10.0
     }
   ],
   "records": []
