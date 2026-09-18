849c0ef030b93e1a154d809f24e45843c154887a
dashboard-publisher <dashboard@localhost>
2026-09-18 08:11:11 +0800

    auto update dashboard

--- duanban_data.js
+++ duanban_data.js
@@ -1,19 +1,8 @@
 window.DUANBAN_DATA = {
-  "generated": "2026-09-17 15:20:06",
-  "today": "20260917",
+  "generated": "2026-09-18 08:10:10",
+  "today": "20260918",
   "emotion": "混沌期",
-  "alerts": [
-    {
-      "time": "14:45:22",
-      "type": "断板买入",
-      "code": "000993",
-      "name": "闽东电力",
-      "breakDate": "20260917",
-      "price": 17.88,
-      "note": "断板日涨幅-3.25%，尾盘低吸",
-      "emotion": "混沌期"
-    }
-  ],
+  "alerts": [],
   "candidates": [
     {
       "code": "000993",
@@ -23,9 +12,9 @@ window.DUANBAN_DATA = {
       "runUp": 77.2,
       "selectDate": "20260917",
       "baseClose": 18.48,
-      "price": 17.9,
-      "pct": -3.14,
-      "sincePct": -3.14,
+      "price": 17.97,
+      "pct": -2.76,
+      "sincePct": -2.76,
       "status": "持有中"
     },
     {
@@ -40,6 +29,58 @@ window.DUANBAN_DATA = {
       "pct": 9.99,
       "sincePct": 9.99,
       "status": "候选"
+    },
+    {
+      "code": "003026",
+      "name": "中晶科技",
+      "boards": 3,
+      "prevClose": 37.0,
+      "runUp": 36.0,
+      "selectDate": "20260918",
+      "baseClose": 37.0,
+      "price": 37.0,
+      "pct": 9.99,
+      "sincePct": 0.0,
+      "status": "候选"
+    },
+    {
+      "code": "001216",
+      "name": "华瓷股份",
+      "boards": 3,
+      "prevClose": 19.88,
+      "runUp": 36.5,
+      "selectDate": "20260918",
+      "baseClose": 19.88,
+      "price": 19.88,
+      "pct": 10.02,
+      "sincePct": 0.0,
+      "status": "候选"
+    },
+    {
+      "code": "603248",
+      "name": "锡华科技",
+      "boards": 3,
+      "prevClose": 22.34,
+      "runUp": 33.1,
+      "selectDate": "20260918",
+      "baseClose": 22.34,
+      "price": 22.34,
+      "pct": 10.0,
+      "sincePct": 0.0,
+      "status": "候选"
+    },
+    {
+      "code": "003001",
+      "name": "中岩大地",
+      "boards": 2,
+      "prevClose": 20.32,
+      "runUp": 30.6,
+      "selectDate": "20260918",
+      "baseClose": 20.32,
+      "price": 20.32,
+      "pct": 10.02,
+      "sincePct": 0.0,
+      "status": "候选"
     }
   ],
   "records": [
@@ -53,9 +94,9 @@ window.DUANBAN_DATA = {
       "emotion": "混沌期",
       "status": "持有中",
       "cost": 17.88,
-      "price": 17.9,
-      "daysAfterBreak": 0,
-      "pnl": 0.11,
+      "price": 17.97,
+      "daysAfterBreak": 1,
+      "pnl": 0.5,
       "buyDate": "20260917",
       "buyPrice": 17.88,
       "buyTime": "14:45:22",
